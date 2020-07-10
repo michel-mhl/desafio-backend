ef27935e22f2a27a2f0267f1205f8e851c1611ed
Michel Gonçalves <michel_mhl@hotmail.com>
2020-07-10 14:57:26 -0300

    Atualizando e excluindo usuario. outros ajustes

--- src/main/java/com/netpos/desafiobackend/resources/UserAccountsResources.java
+++ src/main/java/com/netpos/desafiobackend/resources/UserAccountsResources.java
@@ -26,8 +26,8 @@ public class UserAccountsResources {
 	private UserAccountsService service;
 		
 	@RequestMapping(value="/{id}",method = RequestMethod.GET)
-	public ResponseEntity<?> find(@PathVariable Integer id) {
-		UserAccounts user = service.buscar(id);
+	public ResponseEntity<UserAccounts> find(@PathVariable Integer id) {
+		UserAccounts user = service.find(id);
 		return ResponseEntity.ok().body(user);
 		
 	}
@@ -37,5 +37,19 @@ public class UserAccountsResources {
 		obj = service.insert(obj);
 		URI uri  = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
 		return ResponseEntity.created(uri).build();
+	}
+	
+	@RequestMapping(value="/{id}",method = RequestMethod.PUT)
+	public ResponseEntity<Void>update(@RequestBody UserAccounts obj,@PathVariable Integer id){
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+		
+	}
+	
+	@RequestMapping(value="/{id}",method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/netpos/desafiobackend/services/UserAccountsService.java
+++ src/main/java/com/netpos/desafiobackend/services/UserAccountsService.java
@@ -3,11 +3,12 @@ package com.netpos.desafiobackend.services;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
 import com.netpos.desafiobackend.domain.UserAccounts;
 import com.netpos.desafiobackend.repositories.UserAccontsRepository;
-import com.netpos.desafiobackend.services.exceptions.*;
+import com.netpos.desafiobackend.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class UserAccountsService {
@@ -15,7 +16,7 @@ public class UserAccountsService {
 	@Autowired
 	private UserAccontsRepository repo;
 
-	public UserAccounts buscar(Integer id) {
+	public UserAccounts find(Integer id) {
 		Optional<UserAccounts> obj = repo.findById(id);
 		return obj.orElseThrow(() -> new ObjectNotFoundException(
 				"Objeto não encontrado! Id: " + id + ", Tipo: " + UserAccounts.class.getName()));
@@ -26,4 +27,19 @@ public class UserAccountsService {
 		return repo.save(obj);
 	}
 
+	public UserAccounts update(UserAccounts obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+	
+	public void delete(Integer id ) {
+		find(id);
+		try {
+		repo.deleteById(id);
+		}
+		catch(DataIntegrityViolationException e) {
+			throw new DataIntegrityViolationException("Nao é possivel excluir usuarios que possui produtos");
+		}
+		
+	}
 }
